df1f7bc7bca219ca81e8530748a00d9a9f62f17a
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-08-31 16:30:43 -0700

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Cell from './Cell'
 
 export default class Matrix extends Component {
   
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    vals.map(val => <Cell value={val}/> )
   )
   
   genMatrix = () => (
@@ -17,5 +18,23 @@ export default class Matrix extends Component {
       </div>
     )
   }
+
   
+}
+
+const defaultCells = [
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00'],
+]
+
+Matrix.defaultProps = {
+  values: defaultCells
 }
