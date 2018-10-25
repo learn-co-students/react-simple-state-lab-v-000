cbedb7450fbf33fafd0cae8764786e8c5f366d37
julianjohannesen <github email address>
2018-10-25 16:22:11 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Cell from './Cell';
 
 export default class Matrix extends Component {
+
+	// When genRow is called, genMatrix will pass into genRow a prop called rowVals. Then when genRow calls Cell, it passes in the current value in this.props.values
+	genRow = (aRowVal) => aRowVal.map((aCellVal,ind) => (
+		<Cell value={aCellVal} index={ind} key={`cell-${ind}`} />
+	));
+	  
+  	// index.js passes into its call to Matrix a prop called  "values" with a value of pattern1 (from data.js). We need the curly braces around this.genRow() b/c we're switching from JSX to plain JS
+  	genMatrix = (vals) => vals.map((rowVals,ind) => (<div className="row" key={`row-${ind}`}>{this.genRow(rowVals)}</div>));
   
-  genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
-  
-  genMatrix = () => (
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  )
-  
-  render() {
-    return (
-      <div id="matrix">
-        {this.genMatrix()}
-      </div>
-    )
-  }
-  
+  	render() {
+    	return (
+      		<div id="matrix">
+        		{this.genMatrix(this.props.values)}
+      		</div>
+    	)
+	  }
+	  
+}
+
+Matrix.defaultProps = {
+	values:  [
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+	],
+}
+	  
+Matrix.propTypes = {
+	values: PropTypes.arrayOf(
+		PropTypes.arrayOf(
+			PropTypes.string
+		)
+	).isRequired,
 }
