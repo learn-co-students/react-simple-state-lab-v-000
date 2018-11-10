1f3921e6c2890c3eaf81dba333197a8aa487a823
tsyaeger <tsyaeger@gmail.com>
2018-11-10 14:25:05 -0800

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -9,6 +9,8 @@ export default class Matrix extends Component {
   genMatrix = () => (
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   )
+
+
   
   render() {
     return (
@@ -17,5 +19,21 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
 }
+
+
+const defaultVals = () => {
+  const mx = []
+  const color = "#F00 "
+  for (var i = 0; i < 10; i++) {
+    let colors = color.repeat(10).split(" ").slice(0, -1)
+    mx.push(colors)
+  }
+  return mx
+}
+
+Matrix.defaultProps = {
+  values: defaultVals()
+}
+
+
