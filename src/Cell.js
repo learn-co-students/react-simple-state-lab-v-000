570c5391c78e40ec7472c946060e87204b53d7d1
arye-dov-eidelman <aryedov.e@gmail.com>
2019-01-08 12:02:56 -0500

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -3,15 +3,11 @@ import React, { Component } from 'react';
 export default class Cell extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      color: this.props.value
-    }
+    this.state = {color: this.props.value}
   }
 
   handleClick = () => {
-    this.setState({
-      color: '#333'
-    })
+    this.setState( {color: '#333'} )
   }
 
   render() {
