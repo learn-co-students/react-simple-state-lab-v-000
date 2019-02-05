acc125285f66f1678a1cc70fcc00eda76863da5f
kev112 <werewaswas@live.com>
2019-02-04 17:58:27 -0800

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import Cell from './Cell';
 
 export default class Matrix extends Component {
   
-  genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
+  genRow = (vals) => {
+    vals.map(val => <Cell value={val} />); // replace me and render a cell component instead!
+  }
   
   genMatrix = () => (
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+    this.props.values.map(rowVals => { 
+      return (
+        <div className="row">
+          {this.genRow(rowVals)}
+        </div> 
+      )}
+    )
   )
   
   render() {
@@ -19,3 +26,14 @@ export default class Matrix extends Component {
   }
   
 }
+
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
+}
+
+
+// Line 11:  Expected to return a value in arrow function                           array-callback-return
+// Line 12:  Expected an assignment or function call and instead saw an expression  no-unused-expressions
