207ab7f1ac67eaed32f3a64a0d6066566d903e03
brennanmrose <brennan.m.rose@gmail.com>
2019-07-13 20:47:28 -0600

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,22 @@
+class Cell extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: {this.props.value}
+    }
+  }
+}
+
+cell = () => {
+  const newColor = #333
+  this.setState({
+    color: newColor
+  })
+
+  render () {
+    return (
+      <div class="cell" onClick={this.cell}></div>
+      // style={{backgroundColor: newColor}}
+    )
+  }
+}
