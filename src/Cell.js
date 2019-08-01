70a388f0c385089f4e0b858bb876413410bf65a1
Taylor Williams <taylor@thewilliams.net>
2019-08-01 10:49:06 +0000

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: props.value
+    }
+  }
+  touch = () => {
+    this.setState({
+      color: "#333"
+    })
+  }
+  render() {
+    return <div className="cell" onClick={this.touch} style={{backgroundColor: this.state.color}}></div>
+  }
+}
