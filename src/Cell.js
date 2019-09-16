310991ab65335ba9b42c38a8799f73dc35bf6c07
Madeline-Stark <madeline.stark1@gmail.com>
2019-09-16 16:21:03 -0700

    Update Cell.js

--- src/Cell.js
+++ src/Cell.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Cell extends Component {
-
   constructor(props) {
-    super(props);
+    //matrix is passing in props
+    //constructor is first thing run in component
+    super(props); //super is what is setting props through the default constructor
     this.state = {
-      color: this.props.value
+      color: props.value //dont need this because accessing props arg
     };
+    // ...define initial state with a key of 'color' set to the 'value' prop
   }
+
+  handleClick = () => {
+    this.setState({
+      color: "#333"
+    });
+  };
 
   render() {
     return (
       <div
-      className = "cell"
-      style={{backgroundColor: this.state.color}}
-      onClick={this.clickListener} //was calling with() and without this and error was reading undefined
-      >
-      </div>
-    )
+        className="cell"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      ></div>
+    );
   }
-
-  clickListener = () => {
-    this.setState({
-      color: '#333'
-    })
-  }
-
 }
