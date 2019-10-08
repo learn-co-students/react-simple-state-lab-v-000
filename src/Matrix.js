9cd69174725dfcb0df4644204f5dfd857234e245
Conrad Bituin <38743840+cbituin@users.noreply.github.com>
2019-10-08 09:09:00 -0500

    Complete

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Cell from "./Cell";
 
 export default class Matrix extends Component {
-  
-  genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
-  
-  genMatrix = () => (
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  )
-  
+  // Maps array => ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+  genRow = vals => vals.map(data => <Cell value={data} />);
+  genMatrix = () =>
+    this.props.values.map(
+      rowVals => <div className="row">{this.genRow(rowVals)}</div>
+      //returns 10x of
+      //= > ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+    );
+
   render() {
-    return (
-      <div id="matrix">
-        {this.genMatrix()}
-      </div>
-    )
+    return <div id="matrix">{this.genMatrix()}</div>;
   }
-  
 }
+
+Matrix.defaultProps = {
+  values: (() => {
+    for (let i = 0; i < 10; i++) {
+      return Array(10).fill("#F00");
+    }
+  })()
+};
