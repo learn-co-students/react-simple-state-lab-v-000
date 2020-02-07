dc2eddaa617303163d50d020fde4b8a03ee95132
github username <github email address>
2020-02-07 06:13:10 -0800

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+
+  constructor(props) {
+    super()
+    this.state = {
+      color: props.value
+    }
+  }
+
+  colorChange = () => {
+    this.setState ({
+      color: '#333'
+    })
+  }
+
+  render() {
+    return (
+      <div className="cell" onClick={this.colorChange} style={{backgroundColor: `${this.state.color}`}} >
+
+      </div>
+    )
+  }
+
+}
