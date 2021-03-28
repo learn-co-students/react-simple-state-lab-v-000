f3cb281a0af567f5e3794d21ad1fff54d5126577
Meg Gutshall <meghan.gutshall@gmail.com>
2021-03-28 15:41:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import './index.css';
 import { pattern1 } from './data.js'
 import Matrix from './Matrix.js'
 
+// Matrix.defaultProps = {
+  // values: defaultPattern
+// }
+
 ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
