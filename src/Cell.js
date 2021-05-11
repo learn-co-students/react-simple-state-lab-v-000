f75cc6c1cf7bfca43a1a2725e96b8aee4ca8d586
emanao <emanao@me.com>
2021-05-11 11:38:43 -0400

    New component Cell. Has state color initially set.

---
+++ src/Cell.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+    constructor(props){
+        super();
+        this.state={
+            color:props.value
+        }
+    }
+    render(){
+        return(
+            <div className="cell"></div>
+        )
+    }
+}
+  
